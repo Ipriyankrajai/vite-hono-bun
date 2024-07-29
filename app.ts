c0620325f52d22d01e1d6a6f8a9027a0f8e27010
Priyank Rajai <priyank73@hotmail.com>
2024-07-29 22:11:05 +0530

    feat: adding logger

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
-import { Hono } from 'hono'
-const app = new Hono()
+import { Hono } from "hono";
+import { logger } from "hono/logger";
 
-app.get('/', (c) => c.text('Hello, Hono!'))
+const app = new Hono();
 
-export default app
+app.use("*", logger());
+app.get("/", (c) => c.text("Hello, Hono!"));
+
+export default app;
