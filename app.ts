46fea4e9564ff2e39f1f525f8a08f320e34f19ca
Priyank Rajai <priyank73@hotmail.com>
2024-07-29 22:25:28 +0530

    fix: import for expenses route

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { expenseRoute } from "./routes/expense";
+import { expenseRoute } from "./routes/expenses";
 
 const app = new Hono();
 
