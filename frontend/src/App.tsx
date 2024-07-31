7d014552111493b4a1bfbfec6b742404bde7059c
Priyank Rajai <priyank73@hotmail.com>
2024-07-31 22:49:44 +0530

    feat: creating a card to display total spent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
 function App() {
+  const [totalSpent, setTotalSpent] = useState(0);
+
+  useEffect(() => {
+    async function getTotalSpent() {
+      const resp = await fetch("/api/expenses/total-spent");
+      const data = await resp.json();
+      setTotalSpent(data.total);
+    }
+    getTotalSpent();
+  }, []);
   return (
     <>
-      <Card>
+      <Card className="max-w-xl m-auto">
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle>Total Spent</CardTitle>
+          <CardDescription>Total amount you have spent</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <p>{totalSpent}</p>
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
       </Card>
     </>
   );
